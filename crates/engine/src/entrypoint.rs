f7dc13d2f835729b52cb9fc13620e93161186195
brurucy <rucy.carneiro@pipedrive.com>
2022-02-12 14:13:05 +0200

    The engine, at last, works, to a certain extent.

--- crates/engine/src/entrypoint.rs
+++ crates/engine/src/entrypoint.rs
@@ -1,76 +1,114 @@
 use crate::types::{
-    DataflowExecutionClosure, Triple, TripleCollection, TripleInputSession, TripleSink,
-    TripleTrace, UnaryMaterialization, WorkerExecutionClosure,
+    AboxMaterialization, Terminator, TripleSink, TripleSource, UnaryMaterialization,
 };
 use differential_dataflow::input::Input;
-use differential_dataflow::lattice::Lattice;
 use differential_dataflow::operators::arrange::ArrangeBySelf;
-use flume::Sender;
 use timely::communication::allocator::Generic;
-use timely::communication::Allocator;
-use timely::dataflow::scopes::Child;
-use timely::dataflow::Scope;
 use timely::worker::Worker;
-use timely::Config;
 
 pub fn reason(
     cfg: timely::Config,
     tbox_materialization: UnaryMaterialization,
-    tbox_sink: TripleSink,
+    abox_materialization: AboxMaterialization,
+    tbox_input_source: TripleSource,
+    abox_input_source: TripleSource,
+    tbox_output_sink: TripleSink,
+    abox_output_sink: TripleSink,
+    terminator: Terminator,
 ) -> () {
     timely::execute(cfg, move |worker: &mut Worker<Generic>| {
-        let (mut tbox_input_session, tbox_trace) =
+        let (mut tbox_input_session, mut tbox_trace) =
             worker.dataflow_named::<usize, _, _>("tbox_materialization", |scope| {
-                let tbox_sink = tbox_sink.clone();
+                let tbox_output_sink = tbox_output_sink.clone();
                 let (tbox_input_session, tbox_collection) =
                     scope.new_collection::<(usize, usize, usize), isize>();
                 let materialization = tbox_materialization(&tbox_collection);
-                materialization.inspect(move |x| tbox_sink.send(*x).unwrap());
+                materialization.inspect(move |x| tbox_output_sink.send(*x).unwrap());
                 (tbox_input_session, materialization.arrange_by_self().trace)
             });
+        let mut abox_input_session =
+            worker.dataflow_named::<usize, _, _>("abox_materialization", |scope| {
+                let abox_output_sink = abox_output_sink.clone();
+                let (abox_input_session, abox_collection) =
+                    scope.new_collection::<(usize, usize, usize), isize>();
+                let tbox = tbox_trace.import(scope).as_collection(|k, _v| *k);
+                let materialization = abox_materialization(&tbox, &abox_collection);
+                materialization.inspect(move |x| abox_output_sink.send(*x).unwrap());
+                abox_input_session
+            });
         if worker.index() == 0 {
-            tbox_input_session.insert((28, 17, 29));
-            tbox_input_session.insert((28, 4, 13));
+            loop {
+                if tbox_input_source.is_full() {
+                    tbox_input_source
+                        .drain()
+                        .for_each(|triple| tbox_input_session.update(triple.0, triple.1));
+                    tbox_input_session.flush();
+                    tbox_input_session.advance_to(*tbox_input_session.epoch() + 1);
+                }
+                if abox_input_source.is_full() {
+                    abox_input_source
+                        .drain()
+                        .for_each(|triple| abox_input_session.update(triple.0, triple.1));
+                    abox_input_session.flush();
+                    abox_input_session.advance_to(*abox_input_session.epoch() + 1);
+                }
+                worker.step();
+                if let Ok(_) = terminator.try_recv() {
+                    break;
+                }
+            }
         };
-        worker.step();
-    });
+    })
+    .unwrap();
 }
 
 #[cfg(test)]
 mod tests {
     use crate::entrypoint::reason;
-    use crate::types::{TripleCollection, UnaryMaterialization, WorkerExecutionClosure};
-    use differential_dataflow::input::Input;
-    use differential_dataflow::operators::arrange::{ArrangeByKey, ArrangeBySelf};
-    use differential_dataflow::operators::{JoinCore, Threshold};
-    use std::borrow::Borrow;
-    use timely::communication::{Allocator, Config};
-    use timely::worker::Worker;
+    use crate::types::TripleCollection;
+    use timely::communication::Config;
 
     #[test]
     fn reason_works() {
-        // These computations are meaningless.
-        // The point of this test is to assert that the closure is indeed executed
-        // And for that, we do some random computations and then just send the output to a channel
-        // We can be sure that the closure was executed if the channel receiver has the expected computation result.
-        let (ttx, trx) = flume::unbounded();
+        let (tbox_output_sink, tbox_output_source) = flume::bounded(2);
+        let (tbox_input_sink, tbox_input_source) = flume::bounded(2);
+        let (abox_output_sink, abox_output_source) = flume::bounded(2);
+        let (abox_input_sink, abox_input_source) = flume::bounded(2);
+        let (termination_sink, termination_source) = flume::bounded(1);
+        tbox_input_sink.send(((28, 17, 29), 1)).unwrap();
+        tbox_input_sink.send(((28, 4, 13), 1)).unwrap();
+        abox_input_sink.send(((30, 28, 1), 1)).unwrap();
+        abox_input_sink.send(((30, 29, 1), 1)).unwrap();
+        termination_sink.send(()).unwrap();
         reason(
             timely::Config {
                 communication: Config::Process(2),
                 worker: Default::default(),
             },
             |tbox: &TripleCollection| return tbox.clone(),
-            ttx,
+            |_tbox: &TripleCollection, abox: &TripleCollection| return abox.clone(),
+            tbox_input_source,
+            abox_input_source,
+            tbox_output_sink,
+            abox_output_sink,
+            termination_source,
         );
 
-        let mut diffs: Vec<((usize, usize, usize), usize, isize)> = vec![];
+        let mut tbox_diffs: Vec<((usize, usize, usize), usize, isize)> = vec![];
+        let mut abox_diffs = tbox_diffs.clone();
 
-        while let Ok(diff) = trx.try_recv() {
-            diffs.push(diff)
+        while let Ok(diff) = tbox_output_source.try_recv() {
+            tbox_diffs.push(diff)
         }
 
-        let expected_diffs = vec![((28, 17, 29), 0, 1), ((28, 4, 13), 0, 1)];
+        while let Ok(diff) = abox_output_source.try_recv() {
+            abox_diffs.push(diff)
+        }
 
-        assert_eq!(expected_diffs, diffs)
+        let expected_tbox_diffs = vec![((28, 17, 29), 0, 1), ((28, 4, 13), 0, 1)];
+        let expected_abox_diffs = vec![((30, 28, 1), 0, 1), ((30, 29, 1), 0, 1)];
+
+        assert_eq!(expected_tbox_diffs, tbox_diffs);
+        assert_eq!(expected_abox_diffs, abox_diffs);
     }
 }
--- crates/engine/src/types.rs
+++ crates/engine/src/types.rs
@@ -2,19 +2,23 @@ use differential_dataflow::input::InputSession;
 use differential_dataflow::operators::arrange::TraceAgent;
 use differential_dataflow::trace::implementations::ord::OrdKeySpine;
 use differential_dataflow::Collection;
-use flume::Sender;
+use flume::{Receiver, Sender};
 use timely::communication::allocator::Generic;
 use timely::communication::Allocator;
 use timely::dataflow::scopes::Child;
-use timely::dataflow::Scope;
 use timely::worker::Worker;
+
+pub type Terminator = Receiver<()>;
 
 pub type Triple = (usize, usize, usize);
 pub type TripleSink = Sender<(Triple, usize, isize)>;
+pub type TripleSource = Receiver<(Triple, isize)>;
 pub type TripleInputSession = InputSession<usize, Triple, isize>;
 pub type TripleTrace = TraceAgent<OrdKeySpine<Triple, usize, isize>>;
 pub type TripleCollection<'b> = Collection<Child<'b, Worker<Generic>, usize>, Triple>;
+
 pub type UnaryMaterialization = for<'a> fn(&TripleCollection<'a>) -> TripleCollection<'a>;
+pub type AboxMaterialization =
+    for<'a> fn(&TripleCollection<'a>, &TripleCollection<'a>) -> TripleCollection<'a>;
+
 pub type WorkerExecutionClosure = Box<dyn Fn(&mut Worker<Allocator>) -> () + Send + Sync + 'static>;
-pub type DataflowExecutionClosure<'a> =
-    Box<dyn FnOnce(&mut Child<'a, Worker<Allocator>, usize>) -> (TripleInputSession, TripleTrace)>;
